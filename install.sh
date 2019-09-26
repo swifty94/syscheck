97b31f71b90f1017606116b1726892e5b6ff8eff
swifty94 <49090543+swifty94@users.noreply.github.com>
2019-09-26 18:01:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,23 @@
 ############################################
 ## Created by Kirill Rudenko ###############
 ############################################
+
+echo "The script is installing. Please hold on"
+sleep 15
+f_install(){
 USR=$(whoami)
 URL='https://raw.githubusercontent.com/swifty94/syscheck/master/syscheck'
-echo "The script is installing. Please hold on"
-sleep 5
 cd /usr/local/bin/
 
-nohup sudo wget $URL -O syscheck
-sudo rm nohup.out
+sudo wget $URL -O syscheck
 sudo chmod +x syscheck
 sudo chown $USR:$USR syscheck
 cd && rm install.sh
+}
+f_install
+sleep 15
 echo "The script was installed successfully"
-sleep 5
+sleep 15
 echo "You can use the command "syscheck" in your terminal"
-sleep 5
+sleep 15
 echo "Enjoy"
